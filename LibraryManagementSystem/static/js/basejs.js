e3de7cbb13d5f14f035f64a885e0296eef4aa63c
mohammadteeti <mohammadteeti@gmail.com>
2023-03-05 16:42:42 +0200

    add static files ,css+js

---
+++ LibraryManagementSystem/static/js/basejs.js
@@ -0,0 +1,16 @@
+
+function refresh (t){
+
+setInterval(function() {
+    location.reload();
+  }, t);
+
+
+  var links = document.getElementsByTagName('link');
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    if (link.rel === 'stylesheet') {
+      link.href += '?reload=' + new Date().getTime();
+    }
+  }
+}
